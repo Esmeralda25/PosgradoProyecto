f702e212afec1817b8161542b939e58e42fafed8
Esmerada Montejo <esmeraldaxdd@gmail.com>
2021-09-11 12:26:21 -0500

    diseño(comprometerse-seguimiento100%), php(comprometerse 50%)

--- resources/views/estudiante/seguimineto.blade.php
+++ resources/views/estudiante/seguimineto.blade.php
@@ -30,64 +30,106 @@
         <div class="row justify-content-center" >
             <div class="col-10">
                 <div class="card col-12">
-                        <div class="card-header" style="text-align: center">
-                            <h1 class="card-title font-weight-bold" style="text-align: center">                                
-                                Seguimiento                         
-                            </h1>
-                        </div>
-                            <form action="" method="POST">        
-                                    <div class="container">
-                                        <div class="card-body">
-                                            <div class="row form-group col-12">
-                                                <label for="" class="row col-12">Titulo</label>
-                                                {{$estudiante->proyecto->titulo}}
-                                            </div>
-            
-                    
-                                            <div class="row form-group col-12">
-                                                <label for="" class="row col-12">Hipotesis</label>
-                                                {{$estudiante->proyecto->hipotesis}}
-                                            </div>
-            
-                
-                                            <div class="row form-group col-12">
-                                                <label for="" class="row col-12">Objetivo General</label>
-                                                {{$estudiante->proyecto->objetivos}}
+                    <div class="card-header">
+                        <h1 class="card-title font-weight-bold" style="font-size: 40px;">Seguimiento</h1>
+                    </div>
 
-                                            </div>
-                                            <div class="row form-group col-12">
-                                                <label for="" class="row col-12">Objetivo Especifico</label>
-                                                {{$estudiante->proyecto->objetivose}}
-                                            </div>
-                                        <div>
-                                        <!-- espacio entre contenido-->
-                                        </div>
-                                        <div>
-                                            <h2 style="width: 100%; text-align:center; background:rgba(0, 0, 0, 0.603); padding:0 0; color:white;margin-top:15px">Compromisos</h2>
-                                        </div>
+                    <div class="card-body">
+                    <table class="table table-striped table-hover">
+                            <thead>
+                                <tr style="text-align: center;background-color: black;">
+                                        <th style="font-size: 25px;">
+                                            Detalles del Proyecto
+                                        </th>
+                                </tr> 
+                            </thead>  
+                            <tbody>
+                                <tr>
+                                    <!-- TITULO -->  
+                                    <th>
+                                        <label for="" style="font-family:Arial; color: white;font-size: 25px;">Título: </label>
+                                        <small style="margin-left: 5px;font-family:Arial;color: white;font-size: 20px;">{{$estudiante->proyecto->titulo}}</small>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <!-- HIPOTESIS -->  
+                                    <th>
+                                        <label for="" style="font-family:Arial;color: white;font-size: 25px;">Hipótesis: </label> 
+                                        <small style="margin-left: 5px;font-family:Arial;color: white;font-size: 20px;">{{$estudiante->proyecto->hipotesis}}</small> 
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <!-- OBJETIVO GENERAL -->  
+                                    <th>
+                                        <label for="" style="font-family:Arial;color: white;font-size: 25px;">Objetivo General: </label>
+                                        <small style="margin-left: 5px;font-family:Arial;color: white;font-size: 20px;">{{$estudiante->proyecto->objetivos}}</small> 
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <!-- OBJETIVO ESPECIFICO --> 
+                                    <th>
+                                        <label for="" style="font-family:Arial;color: white;font-size: 25px;">Objetivo Específico:  </label>
+                                        <small style="margin-left: 5px;font-family:Arial;color: white;font-size: 20px;">{{$estudiante->proyecto->objetivose}}</small>
+                                    </th> 
+
+                                </tr>
+                                                              
+                            </tbody>        
+                        </table>
+                    
+                        <div style="height:20px;"></div>
+                        <!-- TABLA DE COMPROMISOS ADQUIRIDOS -->  
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr class="row col-12" style="text-align: center;">
+                                        <th class="col-12" style="font-size: 25px;">
+                                            Compromisos Adquiridos
+                                        </th>
+                                </tr> 
+                            </thead>  
+                            <tbody>
+                                <tr>
+                                    <th>
                                         @forelse ($estudiante->proyecto->compromisos( $estudiante->semestreActual->id )->get() as $compromiso)
-                                            <li>{{$compromiso->que}}, se programo:  {{$compromiso->cuantos_prog}}</li>
+                                                <li>{{$compromiso->que}}, se programo: {{$compromiso->cuantos_prog}}</li>
                                         @empty
-                                            Sin compromisos definidos para este semestre
+                                                Sin compromisos definidos para este semestre
                                         @endforelse
-                                        <div>
-                                            <h2 style="width: 100%; text-align:center; background:rgba(0, 0, 0, 0.603); padding:0 0; color:white;margin-top:15px">Actividades</h2>
-                                        </div>
+                                    </th>
+                                </tr>
+                                                              
+                            </tbody>        
+                        </table>
+                        <div style="height:15px;"></div>
+                        <!-- TABLA DE ACTIVIDADES ADQUIRIDOS style="margin-right: 5px; margin-left:4px"-->  
+                        <table class="table table-dark table-striped">
+                            <thead>
+                                <tr class="row col-12" style="text-align: center;">
+                                        <th class="col-12" style="font-size: 25px;">
+                                            Actividades Adquiridas
+                                        </th>
+                                </tr> 
+                            </thead>  
+                            <tbody>
+                                <tr>
+                                    <th>
                                         @forelse ($estudiante->proyecto->actividades( $estudiante->semestreActual->id )->get() as $actividad)
-                                            <li>{{$actividad->nombre}} - en el periodo "{{$actividad->periodo}}"</li>
+                                            <li>{{$actividad->nombre}} - en el periodo:  "{{$actividad->periodo}}"</li>
                                         @empty
                                             Sin actividades definidas para este semestre
                                         @endforelse
+                                    </th>
+                                </tr>
+                                                              
+                            </tbody>        
+                        </table>
 
-{{--
-                                        <div>
-                                            <button class="btn btn-danger"><a href="{{url('/compromisos')}}" onclick="alerta()" style="color: black">Aceptar</a></button>
-                                        </div>
---}}                                    
-        
-                                    </div>
-                                </div>
-                            </form>
+                       <div>
+                            <button class="btn btn-danger" style="margin: 15px;"><a href="{{url('/estudiantes')}}" onclick="alerta()" style="color: black">Aceptar</a></button>
+                        </div>
+                                      
+                    </div>
+                            
                         </div>
                     
                     </div>
