ca6b0d4033f0c8063502b57bc5e5eada90c36a68
jogs78 <jogs78@gmail.com>
2022-09-09 10:05:56 -0500

    Modificaciones en seeders

--- database/seeders/GeneracionesSeeder.php
+++ database/seeders/GeneracionesSeeder.php
@@ -15,12 +15,12 @@ class GeneracionesSeeder extends Seeder
     public function run()
     {
       $generaciones = array(
-        array('id' => '5','nombre' => 'Primer Genacion Maes','descripcion' => 'Sexto periodo','pes_id' => '2'),
-        array('id' => '6','nombre' => 'Segunda Genacion Periodo Doc','descripcion' => 'Noveno periodo','pes_id' => '2'),
-        array('id' => '7','nombre' => '2020 - 2022','descripcion' => 'Alumnos que entran durante la pandemia','pes_id' => '5'),
-        array('id' => '8','nombre' => '2021 - 2023','descripcion' => 'Alumnos que entran durante la pandemia 21','pes_id' => '5'),
-        array('id' => '9','nombre' => 'Generacion 2020 - 2023','descripcion' => 'Entraron en 2020','pes_id' => '6'),
-        array('id' => '10','nombre' => 'Generacion 2021 - 2024','descripcion' => 'Entraron en 2021.','pes_id' => '6')
+        array('id' => '5','nombre' => 'Primer Genacion Maes','descripcion' => 'Sexto periodo','pe_id' => '2'),
+        array('id' => '6','nombre' => 'Segunda Genacion Periodo Doc','descripcion' => 'Noveno periodo','pe_id' => '2'),
+        array('id' => '7','nombre' => '2020 - 2022','descripcion' => 'Alumnos que entran durante la pandemia','pe_id' => '5'),
+        array('id' => '8','nombre' => '2021 - 2023','descripcion' => 'Alumnos que entran durante la pandemia 21','pe_id' => '5'),
+        array('id' => '9','nombre' => 'Generacion 2020 - 2023','descripcion' => 'Entraron en 2020','pe_id' => '6'),
+        array('id' => '10','nombre' => 'Generacion 2021 - 2024','descripcion' => 'Entraron en 2021.','pe_id' => '6')
       );
             
         foreach ($generaciones  as $generacion ) {
