b5cd8be5f60a98295c066aaf3a9fd39cc689ee9e
Miguel de Jesus Salgado Alcala <migueljesus2009@live.com.mx>
2022-10-06 15:02:19 -0500

    se agregaro las rutas para mostrar la tabla estudiantes, vista modal y actualizar periodo

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PeriodoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/periodos/{periodo}/periodoEstudianteGet', [PeriodoController::class, 'periodoEstudianteGet'])->name('periodos.periodoEstudianteGet');
+Route::get('/periodos/{periodo?}/periodoEstudianteAsignar', [PeriodoController::class, 'periodoEstudianteAsignar'])->name('periodos.periodoEstudianteAsignar');
+Route::patch('/periodos/{periodo?}/periodoEstudianteAsignarPatch', [PeriodoController::class, 'periodoEstudianteAsignarPatch'])->name('periodos.periodoEstudianteAsignarPatch');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
