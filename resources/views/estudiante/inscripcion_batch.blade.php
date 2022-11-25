e661860f59e6a88503ae3096f88c71a1c006b4de
Miguel de Jesus Salgado Alcala <migueljesus2009@live.com.mx>
2022-11-24 18:59:29 -0600

    creacrion de la vista batch para subir archivos excel

---
+++ resources/views/estudiante/inscripcion_batch.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+@section('content')
+        <div style="height:60px">
+        </div>  <!-- espacio del top -->  
+        
+        <div class="row justify-content-center" >
+            <div class="col-10">
+                <div class="card col-12">
+                    <div class="card-header" style="text-align: center">
+                        <h5 class="card-title font-weight-bold" style="font-size: 30px;">Registrar Estudiantes</h5><br>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form action="{{route('periodos.importarExcel',$periodo->id)}}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    @if(Session::has('message'))
+                                    <p>{{Session::get('message')}}</p>
+                                    @endif
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Importar Registro de Estudiantes</label>
+                                        <input id="archivo" name="archivo" type="file"  style="width: 100%" value="">
+                                    </div>
+                                    <button type="submit" class="btn btn-warning" "4"><a>Guardar</a></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div><!-- card -->
+            </div><!-- col10 -->
+        </div><!-- row -->
+@endsection
